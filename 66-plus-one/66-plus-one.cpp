f5e5ca902c0e948b378e782d633a33bd022dc1ea
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-08-26 01:01:09 +0600

    Time: 0 ms (100.00%), Space: 8.8 MB (12.81%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,22 +1,20 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int sz = digits.size()-1;
-        int flg = 1;
-        for(int i=sz; i>=0; i--){
-            int chk = digits[i]+flg;
+        int n = digits.size()-1;
+        int flg = 1, chk;
+        for(int i=n; i>=0; i--){
+            chk = digits[i]+flg;
             if(chk>9){
-                digits[i] = chk%10;
-                flg = chk/10;
+                digits[i] = 0;
+                flg = 1;
             }
             else{
                 digits[i] = chk;
-                flg=0;
+                flg = 0;
             }
         }
-        if(flg>0){
-            digits.insert(digits.begin(), flg);
-        }
+        if(flg>0) digits.insert(digits.begin(), flg);
         return digits;
     }
 };
