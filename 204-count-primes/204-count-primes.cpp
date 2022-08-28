e7a6a4ed416fd9dd35815c536e79d942abd5026b
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-08-28 14:31:49 +0600

    Time: 168 ms (97.98%), Space: 10.3 MB (70.50%) - LeetHub

--- 204-count-primes/204-count-primes.cpp
+++ 204-count-primes/204-count-primes.cpp
@@ -1,23 +1,22 @@
 class Solution {
 public:
     int sieve(int n){
-        vector<int> vis(n+1, 0);
-        int cnt = 0;
+        if(n<2){
+            return 0;
+        }
+        vector<bool> vis(n+1, false);
+        int cntComposite = 0;
         for(int i=2; i*i<=n; i++){
-            if(vis[i]==0){
-                for(int j=i+i; j<n; j+=i){                    
-                    if(vis[j]==0){
-                        vis[j] = 1;
-                        cnt++;
+            if(vis[i]==false){
+                for(int j=i*i; j<n; j+=i){                    
+                    if(vis[j]==false){
+                        vis[j] = true;
+                        cntComposite++;
                     }
                 }
             }
         }
-        // for(int i=2; i<n; i++){
-        //     //cout << 
-        //     if(vis[i]==0) cnt++;
-        // }
-        return n<2?0:(n-1)-(cnt+1);
+        return (n-1)-(cntComposite+1);
     }
     int countPrimes(int n) {
         return sieve(n);
