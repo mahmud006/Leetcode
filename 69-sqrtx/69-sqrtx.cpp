587289f2e73a6944a6fb6021a66eb505325406cb
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-08-29 00:30:23 +0600

    Time: 3 ms (72.09%), Space: 5.8 MB (73.98%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long L = 0, R = x;
-        while(L<R){
-            long long mid = L + (R-L+1)/2;
-            if(mid*mid>x){
-                R = mid-1;
-            }
-            else{
-                L = mid;
-            }
+        if(x==0) return 0;
+        double xN = x;
+        int cnt = 20;
+        while(cnt--){
+            //newton raphson
+            xN = ((xN*xN)+x)/(xN+xN);
         }
-        return R;
+        return xN;
     }
 };
