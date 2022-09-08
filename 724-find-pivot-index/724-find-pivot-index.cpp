437021373cecae3b676a6eb99fc16199a4d569ac
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-09-08 20:58:33 +0600

    Time: 122 ms (6.95%), Space: 31.2 MB (18.35%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int i = 1, j = nums.size()-2;
+        int arr1[nums.size()], arr2[nums.size()];
+        arr1[0] = nums[0], arr2[nums.size()-1] = nums[nums.size()-1];
+        while(i<=nums.size() && j>=0){
+            arr1[i] = nums[i] + arr1[i-1];
+            arr2[j] = nums[j] + arr2[j+1];
+            i++;
+            j--;
+        }
+        for(int i=0; i<nums.size(); i++){
+            cout << arr1[i] << " "<<arr2[i] <<endl;
+            if(arr1[i]==arr2[i]) return i;
+        }
+        return -1;
+    }
+};
