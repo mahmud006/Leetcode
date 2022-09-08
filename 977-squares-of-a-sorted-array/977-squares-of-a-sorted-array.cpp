9805666e4f156476963de812c0ceba5334ac66bc
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-09-08 19:31:48 +0600

    Time: 51 ms (60.38%), Space: 27 MB (16.24%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -1,10 +1,41 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
+        int mn = INT_MAX;
+        int idx;
         for(int i=0; i<nums.size(); i++){
-            nums[i] = nums[i]*nums[i];
+            if(abs(nums[i]*nums[i])<mn){
+                mn = abs(nums[i]*nums[i]);
+                idx = i;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        vector<int> arr;
+        int L = idx-1, R = idx+1;
+        arr.push_back(mn);
+        int j = 0;
+        cout << L << " " << R <<endl;
+        while(L>=0 && R<=nums.size()-1){
+            if(nums[L]*nums[L] <= nums[R]*nums[R]){
+                cout << nums[L];
+                arr.push_back(nums[L]*nums[L]);
+                j++;
+                L--;
+            }
+            else if(nums[L]*nums[L] > nums[R]*nums[R]){
+                cout << nums[R];
+                arr.push_back(nums[R]*nums[R]);
+                j++;
+                R++;
+            }
+        }
+        while(L>=0){
+            arr.push_back(nums[L]*nums[L]);
+            L--;
+        }
+        while(R<nums.size()){
+            arr.push_back(nums[R]*nums[R]);
+            R++;
+        }
+        return arr;
     }
 };
