98c7ae354d392539ffbda600272b4a8ad37b6b6b
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-09-27 22:53:11 +0600

    Time: 498 ms (38.02%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-Select email AS Email from Person Group By email having COUNT(email) > 1
+Select email AS Email from Person Group By email having COUNT(id) > 1
