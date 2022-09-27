9c30254859495b4b5695237fe76855c1496160d5
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-09-27 11:48:36 +0600

    Time: 3 ms (88.14%), Space: 11.6 MB (77.57%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.cpp
+++ 22-generate-parentheses/22-generate-parentheses.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     vector<string> res;
-    void generate(string curr, int open, int close){
+    void generate(string &curr, int open, int close){
         if(open==close && open==0){
             res.push_back(curr);
             return;
         }
-        if(open > 0) generate(curr + "(", open-1, close);
-        if(open < close) generate(curr + ")", open, close-1);
+        if(open > 0) {
+            curr.push_back('(');
+            generate(curr, open-1, close);
+            curr.pop_back();
+        }
+        if(open < close) {
+            curr.push_back(')');
+            generate(curr, open, close-1);
+            curr.pop_back();
+        }
     }
     vector<string> generateParenthesis(int n) {
         res.clear();
-        generate("", n, n);
+        string curr = "";
+        generate(curr, n, n);
         return res;
     }
 };
