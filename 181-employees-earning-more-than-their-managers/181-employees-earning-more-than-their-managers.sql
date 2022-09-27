19689738473b1d3b89080cd4f546838d3ea927d5
Mahmudul Hasan <mh.mahmud006@gmail.com>
2022-09-27 22:08:08 +0600

    Time: 798 ms (18.29%), Space: 0B (100.00%) - LeetHub

--- 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
+++ 181-employees-earning-more-than-their-managers/181-employees-earning-more-than-their-managers.sql
@@ -1,2 +1,3 @@
 # Write your MySQL query statement below
-Select name AS Employee from Employee AS emp where salary > (Select salary from Employee AS manager where emp.managerId = manager.id)
+# Select name AS Employee from Employee AS emp where salary > (Select salary from Employee AS manager where emp.managerId = manager.id)
+Select Emp.name AS Employee from Employee AS emp join Employee AS manager on Emp.managerId = manager.id where emp.salary > manager.salary;
